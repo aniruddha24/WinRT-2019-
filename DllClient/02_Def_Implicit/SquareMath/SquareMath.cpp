b70074699cba9d9e7853d290a6754fdec06ffeb5
aniruddha24 <aniruddhavanjari22@gmail.com>
2019-08-18 19:38:42 +0530

    9.1 - DllClient ,Four types DllClient

---
+++ DllClient/02_Def_Implicit/SquareMath/SquareMath.cpp
@@ -0,0 +1,81 @@
+//Header files 
+#include <windows.h>
+#include "MyMath.h"
+
+//gloabal function declation
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+#pragma comment(lib,"MyMath.lib")
+//WinMain()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//variable declaration
+	WNDCLASSEX wndclass;
+	HWND hwnd;
+	MSG msg;
+	TCHAR szAppName[] = TEXT("MyApp");
+
+	//code
+	//initialization of WNDCLASSEX
+	wndclass.cbSize = sizeof(WNDCLASSEX);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.lpszClassName = szAppName;
+	wndclass.lpszMenuName = NULL;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//register above class 
+	RegisterClassEx(&wndclass);
+
+	//Create Window
+	hwnd = CreateWindow(szAppName,
+		TEXT("My Application"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	//message loop
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return((int)msg.wParam);
+
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	int num = 5, square;
+	TCHAR str[255];
+	//code 
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		square = MakeSquare(num);
+		wsprintf(str, TEXT("Square of %d is %d"), num, square);
+		MessageBox(hwnd, str, TEXT("Message"), MB_OK);
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+
